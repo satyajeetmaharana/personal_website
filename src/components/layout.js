d970d5be23ccc150e24f3229dbfb7f9abbe6d30e
Satyajeet Maharana <sm8235@nyu.edu>
2020-01-21 16:40:42 -0500

    Made responsive

--- src/components/layout.js
+++ src/components/layout.js
@@ -66,8 +66,9 @@ const Layout = ({ children }) => {
       <div style={{position:'fixed',width:'100%',height:'100%',top:'0',bottom:'0',left:'0',right:'0',backgroundColor:'rgba(0,0,0,0.5)'}}>
       </div>
       <Desktop>
-        <div style={{position:'absolute',top:'50%',left:'50%',transform:'translate(-50%,-50%)', textAlign:'center'}}>
-          
+
+      
+        <div className='intro-class'>
             <div style={headingStyle}>Hi, I'm Satyajeet.</div>
             <div style={{fontSize:'1em',color:'white',fontWeight:'500',textShadow: '2px 2px 4px #000000'}}>Software Engineer | New York City</div>
             <div>
